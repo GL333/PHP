93bb9e5d52e820f1121672d0d450f2a1b98e210d
GL333 <giedriusgl@yahoo.com>
2018-04-17 16:51:50 +0300

    4pamoka nd

---
+++ 4pamoka/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php session_start(); ?>
+
+<form action="users.php" method="POST">
+    User: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="username" id="">
+    <br/>
+    Password: <input type="password" name="password" id="">
+    <br/>
+    <input type="submit" value="Prisijungti" name="login">
+</form>
+<?php
+if (!empty($_SESSION['username']))
+    echo "Welcome back ".$_SESSION['username'];
+?>
+
+</body>
+</html>
