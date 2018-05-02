587e1196670b0fcaa4d2afe58b4bfcd7530c2279
GL333 <giedriusgl@yahoo.com>
2018-05-02 17:29:16 +0300

    9nd

--- oop/1pamoka/index.php
+++ oop/1pamoka/index.php
@@ -1,23 +1,80 @@
 <?php
-class Person
+class Post
 {
-    public $legs = 2;
-    function walk(){
-        echo "i have: ".$this->legs." legs";
-//        $this->step(10);
-    }
-    public function setLegs(){
+    const MAX_LENGTH = 500;
+    private $title = "";
+    private $content = "";
+    private $author = "";
 
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
     }
-    public function getLegs(){
-        return $this -> legs;
+
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+        if (strlen($content) > 500) {
+            echo "Max length is:" . self::MAX_LENGTH;
+        };
     }
-    function step($count){
-        for($i = 0; $i < $count; $i++){
-            echo "Steps...".$i."<br/>";
-        }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
     }
 }
-$first_person = new Person;
-$first_person -> walk();
-echo "This person has: ".$first_person->getLegs()." legs";
+
+class Person
+{
+    private $id = "";
+    private $name = "";
+
+
+    public function setId($id){
+        $this->id = $id;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+$person = new Person();
+$person->setName("Jonas");
+$person->setId(1);
+$post = new Post();
+$post->setTitle("Mano antraste");
+$post->setContent("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s");
+$post->setAuthor($person);
+var_dump($post);
+
+
+
