202ccd77aba30daa2cb87730b0255006ad5f3268
GL333 <giedriusgl@yahoo.com>
2018-05-16 12:27:55 +0300

    14 nd be web serviso

---
+++ oop/6pamoka/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'vendor/fzaninotto/faker/src/autoload.php';
+
+function getDb(){
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "namu_darbas";
+    $dsn = "mysql:host=$host;dbname=$db";
+    return new PDO($dsn, $user, $password);
+}
+
+for($i=0;$i<1000;$i++ ){
+    $faker = Faker\Factory::create();
+    $date = Faker\Provider\DateTime::dateTime();
+
+    $sql = "INSERT INTO comments (author, comment, created_at  ) VALUES (:author, :comment, :created_at )";
+    $sth = getDb()->prepare($sql);
+    $sth->execute($data = [
+        'author' => $faker->name,
+        'comment' => $faker->text,
+        'created_at' => $date->format('Y-m-d-H-i-s')
+    ]);
+
+}
