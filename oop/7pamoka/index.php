b1740643f36731ddc97b2271172daea36d09dbfe
GL333 <giedriusgl@yahoo.com>
2018-05-17 14:54:01 +0300

    15 nd

--- oop/7pamoka/index.php
+++ oop/7pamoka/index.php
@@ -12,6 +12,45 @@ use PHPMailer\PHPMailer\Exception;
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=t9qfc8x8jybwwt9xefjcqxt17qbl4z8kq3737a1m429ksg8a"> </script>
+        <script>
+
+            tinymce.init({
+                selector: 'textarea',
+                height: 500,
+                menubar: false,
+                plugins: [
+                    'advlist autolink lists link image charmap print preview anchor textcolor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table contextmenu paste code help wordcount'
+                ],
+                toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+                content_css: [
+                    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                    '//www.tinymce.com/css/codepen.min.css']
+            });
+        </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </head>
 <body>
 <div class="container">
@@ -21,11 +60,11 @@ use PHPMailer\PHPMailer\Exception;
             <form action="" method="POST">
                 <div class="form-group">
                     <label for="to">Email:</label>
-                    <input type="email" class="form-control" name="to" id="to" placeholder="name@example.com" value="tavo_pastas@pasto_adreas.lt">
+                    <input type="email" class="form-control" name="to" id="to" placeholder="name@example.com" value="giedriusgl@yahoo.com" multiple >
                 </div>
                 <div class="form-group">
                     <label for="subject">Subject:</label>
-                    <input type="text" class="form-control" name="subject" id="subject" placeholder="ivesti antraste">
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="enter subject">
                 </div>
                 <div class="form-group">
                     <label for="content">Enter email body here:</label>
@@ -33,18 +72,17 @@ use PHPMailer\PHPMailer\Exception;
                 </div>
                 <input type="submit" name="send" class="btn btn-primary" value="Send Email">
             </form>
-        </div>
+        </div> <!-- end form block -->
         <div class="col-md-4"></div>
     </div>
 </div>
-
-
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
-
 <?php
 if(isset($_POST['send'])){
     if(!empty($_POST['to']) && !empty($_POST['content']) && !empty($_POST['subject'])  ){
@@ -57,31 +95,60 @@ if(isset($_POST['send'])){
             <div class="alert alert-danger" role="alert">
                 Neišskrido balandis :-(.
             </div>
-        <?php endif; ?>
+        <?php else: ?>
+
+        <?php        function getDb()
+               {
+                   $host = "localhost";
+                   $user = "root";
+                   $password = "";
+                   $db = "namu_darbas";
+                   $dsn = "mysql:host=$host;dbname=$db";
+                   return new PDO($dsn, $user, $password);
+                }
+
+                $sql = "INSERT INTO emails (to_send, subject, content  ) VALUES (:to_send, :subject, :content )";
+                $sth = getDb()->prepare($sql);
+                $sth->execute($data = [
+                    'to_send' => $_POST['to'],
+                    'subject' => $_POST['subject'],
+                    'content' => $_POST['content']
+                ]);
+//            $sth->debugDumpParams();
+        ?>
+
         <div class="alert alert-success" role="alert">
-            Laiškas sėkimingai iškeliavo!
+            Laiškas sėkimingai iškeliavo. Maladiec!
         </div>
+        <?php endif; ?>
         <?php
     }
 }
 function sendMail(Array $data){
-    $mail = new PHPMailer(true);
+    $mail = new PHPMailer(true); // Passing `true` enables exceptions
     try {
         //$mail->SMTPDebug = 2;                                 // Enable verbose debug output
         $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = '';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = '';                 // SMTP username
-        $mail->Password = '';                           // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;
+        $mail->Host = 'smtp.elasticemail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'giedriusgl@yahoo.com';
+        $mail->Password = '4b13dc50-58a3-4cf2-8c09-f016392b3d49';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 2525; // TCP port to connect to
 
-        //$mail->SMTPDebug = 2;                                   // TCP port to connect to
+        //$mail->SMTPDebug = 2;
         //is kur laiskas
-        $mail->setFrom('');
+        $mail->setFrom('giedriusgl@yahoo.com');
 
         //Kam laiskas
-        $mail->addAddress($data['to']);
+        $sendTo = [$data['to']];
+        foreach ($sendTo as $val) {
+            $value = explode(",", $val);
+            foreach ($value as $item) {
+                $mail->addAddress($item);
+            };
+        };
+
         //Kam atsakyti
         $mail->addReplyTo('noreply@noreply.re', 'No reply');
         //Content
